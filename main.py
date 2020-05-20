3496a02c8804ad966512283fe0eb299e560fc43f
windxuemo <wudiwangzun@163.com>
2020-05-20 19:48:28 +0800

    modify main nsisanalyse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 from flask import Flask, request
 from flask_restful import Resource, Api
-#ifrom sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from json import dumps
 import json
 from flask_jsonpify import jsonify
@@ -13,28 +13,56 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 import requests
 import shutil
+import pickle
+import psutil
+import atexit
+import pdb
 sys.path.append('..')
-# import autoinstall.nsisAutoInstall as aInstall
+# import autoinstall.AutoInstall as aInstall
 # import staticDector.nsisStaticDector as sDector
 # import nsisfuzz.nsisAutoFuzz as nsisFuzz
+
 import nsisAnalyse
 
-import pdb
 
 app = Flask(__name__,static_folder='static')
 api = Api(app)
 webDirPath, webfileName = os.path.split(os.path.abspath(__file__))
 executor = ThreadPoolExecutor(max_workers=5)
-webUrl="http://192.168.3.19:8988"
-workUrl="http://192.168.101.10:8000"
-staticInterface='/api/ReceiveData/ScanData'
-installInterface='/api/ReceiveData/InsertData'
+webUrl="http://10.1.1.24:80"
+workUrl="http://192.168.3.23:1080"
+staticInterface='/api/ReceiveScan/ScanData'
+installInterface='/api/ReceiveInsert/InsertData'
 fuzzInterface='/api/ReceiveData/FuzzData'
-CFGInterface='/api/ReceiveData/CFGData'
-taintInterface='/api/ReceiveData/TaintData'
+CFGInterface='/api/ReceiveScan/CFGData'
+taintInterface='/api/ReceiveTaint/Taint'
+symbolicInterface='/api/ReceiveSymbolic/SymbolicData'
 dotFileRelativePath='/static/dotfile/'
-logging.basicConfig(level=logging.INFO)
+networkfuzz_interface='/api/ReceiveNetworkFuzz/NetworkFuzz'
+
+
+store_file="target_list.dict"
 targetDict= {}
+TOKEN="test_token"
+WORK_URL=""
+
+def locate_id(pattern,str_list,id_list):
+    '''
+    pattern 搜索字符串,默认为返回的json文件名
+    '''
+    search_len=len(str_list)
+    for i in range(search_len):
+        if pattern.find(str_list[i])!=-1:
+            logging.debug("find %s id is %s"%(str_list[i],id_list[i]))
+            break
+        else:
+            pass
+    if i<search_len:
+        return id_list[i]
+    else:
+        logging.error("locate id from str_list failed")
+        return -1
+
 def addDict(key,value):
     #维护targetDict
     logging.info(targetDict)
@@ -46,8 +74,7 @@ def addDict(key,value):
     else:
         logging.error("此安装文件已在对应表中,目标字典插入失败")
         return False
-
-
+    
 def delDict(key):
     #删除targetDict中的某个key
     try:
@@ -60,19 +87,170 @@ def postData(postUrl,result):
     logging.info(r.status_code)
     #print(jsonify(result))
 
-def backgroundAnalyse(pluginScript, clientScript, id, args):
+
+
+def backgroundInstall(id):
+    #后台实现自动化安装的函数
+    try:
+        path=targetDict[id]
+        logging.info(path)
+        installResult=aInstall.autoInstall(path)
+    except Exception as e:
+        logging.error(e)
+    postResult={'id':id,'status':'ok','action':'install','result':installResult}
+    logging.debug(json.dumps(postResult,indent=4))
+    postData(webUrl+installInterface,postResult)
+
+def backgroundInstall_url(url):
+    #后台实现自动化安装的函数
+    try:
+        installResult=aInstall.autoInstall(url)
+    except Exception as e:
+        logging.error(e)
+    logging.info(installResult)
+    postResult={'id':id,'status':'ok','action':'install','result':installResult}
+    postData(webUrl+installInterface,postResult)
+
+def backgroundCFG(cfg_list):
+    '''
+    cfg_list : [
+        {
+            "id":"1",
+            "path":"c:/user/test/a.exe"
+        }
+        ]
+    '''
+    try:
+        logging.debug(cfg_list)
+        for each_dict in cfg_list:
+            tid=each_dict['id']
+            logging.debug("before enter cfg generate")
+            logging.debug(each_dict)
+            result_dir=sDector.CfgGenerate(each_dict['path'])
+            logging.debug(result_dir)
+            for each_dot_file in os.listdir(result_dir):
+                each_dot_file=os.path.join(result_dir,each_dot_file)
+                logging.debug("reading %s"%(each_dot_file))
+                dot_fp=open(each_dot_file,'r')
+                dot_content=dot_fp.read()
+                dot_result={
+                    'id':tid,'action':'CFG',
+                'fileName':os.path.basename(each_dot_file),
+                'fileInfo':dot_content
+                }
+                logging.debug( json.dumps(dot_result,indent=4))
+                postData(webUrl+CFGInterface,dot_result)
+    except Exception as e:
+        logging.error(e)
+    logging.debug("exit backgroundCFG")
+
+def backgroundStatic(raw_target_list):
+    '''
+    #后台实现静态漏洞扫描的函数
+    raw_target_list: 由web直接提取出来的需要扫描的文件list以及其id
+    [
+        {
+            "fId": "01-1", //子体ID
+            "fPath": "c:/123.exe"
+        },
+        {
+            "fId": "01-2", //子体ID
+            "fPath": "c:/123.dll"
+        }
+    }
+    '''
+    logging.debug(raw_target_list)
+    target_list=[]
+    target_id_list=[]
+    tmp_cfg_list=[]
+    try:
+        #if targetDict.has_key(id):
+        #    anaylsePath=targetDict[id]
+        all_dector_len=len(raw_target_list)
+        for each_dector_dict in raw_target_list:
+            target_list.append(each_dector_dict['fPath'])
+            target_id_list.append(each_dector_dict['fId'])
+        staticResultList=sDector.staticDector(target_list)
+        #TODO:还没弄好
+    except Exception as e:
+        logging.error(e)
+    logging.debug(staticResultList)
+    for curResultPath in staticResultList:
+        logging.debug(curResultPath)
+        with open(curResultPath,'r') as f:
+            staticResult=json.loads(f.read())
+        tmp_id=locate_id(os.path.basename(curResultPath),target_list,target_id_list)
+        #logging.info(staticResult)
+        staticResult['id']=tmp_id
+        #for debugger
+        staticResult['action']='static'
+        staticResult['status']='ok'
+        logging.debug('Before postdata')
+        logging.debug(json.dumps(staticResult,indent=4) )
+        postData(webUrl+staticInterface,staticResult)
+        logging.debug('After postdata')
+        tmp_save_path=curResultPath.strip(".json")#去掉Json名得到存储路径
+        tmp_cfg_dict={}
+        tmp_cfg_dict['id']=tmp_id
+        tmp_cfg_dict['path']=tmp_save_path
+        tmp_cfg_list.append(tmp_cfg_dict)
+    logging.info('start CGFinterface')
+    executor.submit(backgroundCFG,tmp_cfg_list)
+    logging.info("backgroundstatic ready to exit")
+
+def backgroundFuzz(id,proc_path,target_ip,target_port,load,protocol,auto_path):
+    #后台调度Fuzz的函数
+    try:
+        logging.debug('start network fuzz')
+        logging.debug("proc_path %s auto_path %s"%(proc_path,auto_path))
+        logging.debug("checking proc_moniter status")
+        #TODO 检查proc_moniter是否正常启动
+        fuzz_template_path=os.path.join(webDirPath,"../nsisfuzz/fuzz_template.py")
+        start_fuzz_cmd="start python %s exp -ip %s -port %s -over %s -proc \"%s\" -auto %s -temp %s"%(
+            fuzz_template_path,target_ip,target_port,load,proc_path,auto_path,protocol
+        )
+        logging.debug(start_fuzz_cmd)
+        #TODO 现在用start 在管理员权限下 启动需要过UAC
+        os.system(start_fuzz_cmd)
+        time.sleep(6)
+        #TODO 检查客户端是否正常启动
+        result={"id":id,"token":TOKEN,"action":"networkfuzz","basic_url":WORK_URL,'status':'ok'}
+        logging.debug(json.dumps(result,indent=4))
+        postData(webUrl+networkfuzz_interface,result)
+    except Exception as e:
+        logging.error(e)
+
+def backgroundAnalyse(nId, pluginScript, clientScript, nPath, args):
     #后台实现自动化分析的函数
     print('backgroundAnalyse')
     try:
-        # procPath =targetDict[id]
-        procPath ='/home/test/self_test/server'
+        procPath = nPath
+        # procPath ='/home/work/temp/work/tftpserversp/tftpserver'
         logging.info(procPath)
         analyseResult=nsisAnalyse.autoAnalyse(pluginScript, clientScript, procPath, args)
     except Exception as e:
         logging.error(e)
     logging.info(analyseResult)
-    # postResult={'id':id,'status':'1','action':'install','result':installResult}
-    # postData(webUrl+installInterface,postResult)
+    postResult={'id':nId,'status':'ok','token': '22222', 'action':'symbolicexec','result': analyseResult}
+    postData(webUrl+symbolicInterface ,postResult)
+
+
+def backgroundTaintAnalyse(nId, pluginScript, clientScript, nPath, args):
+    #后台实现自动化分析的函数
+    logging.info('backgroundTaintAnalyse')
+    try:
+        procPath = nPath
+        # procPath ='/home/work/temp/work/tftpserversp/tftpserver'
+        logging.info(procPath)
+        analyseResult=nsisAnalyse.autoAnalyse(pluginScript, clientScript, procPath, args)
+    except Exception as e:
+        logging.error(e)
+    logging.info(analyseResult)
+    postResult={'id':nId,'status':'ok','token': '22222', 'action':'taint','result': analyseResult}
+    logging.info(postResult)
+    postData(webUrl+taintInterface, postResult)
+
+
 
 
 @app.route('/')
@@ -106,14 +284,14 @@ def down():
             result={'id':nId,'status':'ID重复!'}
             return jsonify(result)
         else:
-            result={'id':nId,'status':'1'}
+            result={'id':nId,'status':'ok'}
             return jsonify(result)
     except KeyError:
         result={'id':nId,'status':'KeyError'}
         return jsonify(result)
 
 @app.route('/dispatch/install',methods=['POST','OPTIONS','GET'])
-def install():
+def install():   
     logging.info(request.headers)
     getJson=request.get_json()
     logging.info(getJson)
@@ -128,8 +306,24 @@ def install():
     else:
         logging.info(targetDict)
         executor.submit(backgroundInstall,nId)
-        result={'id':nId,'status':'1','action':'install'}
+        result={'id':nId,'status':'ok','action':'install'}
         return jsonify(result)
+
+@app.route('/dispatch/url_install',methods=['POST','OPTIONS','GET'])
+def url_install():
+    #TODO 留出独立调试自动安装的接口   
+    logging.info(request.headers)
+    getJson=request.get_json()
+    logging.info(getJson)
+    try:
+        nurl=getJson['url']
+    except Exception as e :
+        logging.error(e)
+    else:
+        executor.submit(backgroundInstall_url,nurl)
+        result={'id':'debug','status':'ok','action':'install'}
+        return jsonify(result)
+
 
 @app.route('/dispatch/sampledown',methods=['POST','GET'])
 def fuzz():
@@ -147,7 +341,7 @@ def fuzz():
         logging.debug(nToken,nAction,nPath,nHash,nId,nScriptId)
         #TODO: 添加样本对应关系
         executor.submit(backgroundFuzz,nPath)
-        result={'id':nId,'status':'1','action':'fuzz'}
+        result={'id':nId,'status':'ok','action':'fuzz'}
         return jsonify(result)
     except KeyError:
         result={'id':'1','status':'KeyError'}
@@ -156,21 +350,38 @@ def fuzz():
 @app.route('/dispatch/dector',methods=['POST','GET'])
 def dector():
     #静态扫描的接口
-    logging.info(request.headers)
+    logging.debug(request.headers)
     getJson=request.get_json()
-    logging.info(getJson)
+    logging.debug(getJson)
     try:
         nAction=getJson['action']
-        nId=getJson['id']
+        #nId=getJson['id']
         nToken=getJson['token']
-        logging.info(nToken,nAction,nId)
+        n_target=getJson['target']
+        logging.debug(n_target)
     except KeyError:
         result={'id':'1','status':'KeyError'}
         return jsonify(result)
     else :
-        executor.submit(backgroundStatic,nId)
-        result={'id':'1','status':'1','action':'static'}
+        executor.submit(backgroundStatic,n_target)
+        result={'id':'1','status':'ok','action':'static'}
         return jsonify(result)
+
+@app.route('/debug/cfg',methods=['POST','GET'])
+def debug_cfg():
+    logging.debug(request.headers)
+    getJson=request.get_json()
+    logging.debug(getJson)
+    try:
+        nlist=getJson['cfg_list']
+    except KeyError:
+        result={'id':'1','status':'KeyError'}
+        return jsonify(result)
+    else :
+        executor.submit(backgroundCFG,nlist)
+        result={'id':'1','status':'ok','action':'static'}
+        return jsonify(result)
+
 
 @app.route('/dispatch/networkfuzz',methods=['POST','GET'])
 def networkfuzz():
@@ -186,15 +397,73 @@ def networkfuzz():
         ntarget_ip=getJson['target_ip']
         ntarget_port=getJson['target_port']
         nprotocol=getJson['protocol']
+        auto_path=getJson['auto_path']
         nload=getJson['load']
-        logging.info(nToken,nAction,nId)
+        logging.info(nToken,nAction,nId)          
     except KeyError:
         result={'id':'1','status':'KeyError'}
         return jsonify(result)
     else :
-        #executor.submit(backgroundStatic,nId)
-        result={'id':nId,'status':'1','action':'networkfuzz'}
+        executor.submit(backgroundFuzz,nId,nproc_path,ntarget_ip,ntarget_port,nload,nprotocol,auto_path)
+        result={'id':nId,'status':'ok','action':'networkfuzz'}
         return jsonify(result)
+
+@app.route('/dispatch/sfuzz',methods=['POST','GET'])
+def sfuzz():
+    #文件模糊测试接口
+    logging.info(request.headers)
+    getJson=request.get_json()
+    logging.info(getJson)
+    try:
+        nAction=getJson['action']
+        nId=getJson['id']
+        #nToken=getJson['token']
+        nproc_path=getJson['proc_path']
+        nseed_path=getJson['seeds_path']
+        ncheck_non=getJson['check_non']
+        logging.info(nToken,nAction,nId)          
+    except KeyError:
+        result={'id':'1','status':'KeyError'}
+        result['actoin']=nAction
+        return jsonify(result)
+    else :
+        executor.submit(background_sfuzz,getJson)#后台实现文件模糊测试的函数，
+        result={'id':nId,'status':'ok','action':'sfuzz'}
+        result['actoin']=nAction
+        return jsonify(result)
+
+@app.route('/dispatch/taint',methods=['POST','OPTIONS','GET'])
+def taint():
+    #安装文件的接口
+    print('taint')
+    logging.info(request.headers)
+    getJson=request.get_json()
+    logging.info(getJson)
+    try:
+        print('===============')
+        # pdb.set_trace()
+        nId=getJson['id']
+        nAction=getJson['action']
+        nPath=str(getJson['proc_path'])
+        nArgs=getJson['proc_args']
+        nPluginScript=getJson['plugin_script']
+        nClientScript=getJson['client_script']
+        # logging.info(nId, nAction, nPath, nArgs, nPluginScript, nClientScript)
+
+        # executor.submit(backgroundAnalyse,nId)
+        # backgroundAnalyse(nPluginScript, nClientScript, nId, nArgs)
+        executor.submit(backgroundTaintAnalyse, nId, nPluginScript, nClientScript, nPath, nArgs)
+        # backgroundTaintAnalyse(nId, nPluginScript, nClientScript, nPath, nArgs)
+
+        result={'id':nId,'status':'ok', 'action': 'taint'}
+        return jsonify(result)
+    except KeyError:
+        result={'id':nId,'status':'KeyError'}
+        logging.info(result)
+        return jsonify(result)
+
+
+
 
 @app.route('/dispatch/symbolic',methods=['POST','OPTIONS','GET'])
 def symbolic():
@@ -205,19 +474,21 @@ def symbolic():
     logging.info(getJson)
     try:
         print('===============')
-        pdb.set_trace()
+        # pdb.set_trace()
         nId=getJson['id']
         nAction=getJson['action']
         nPath=str(getJson['proc_path'])
         nArgs=getJson['proc_args']
         nPluginScript=getJson['plugin_script']
         nClientScript=getJson['client_script']
-        logging.info(nId, nAction, nPath, nArgs, nPluginScript, nClientScript)
+        # logging.info(nId, nAction, nPath, nArgs, nPluginScript, nClientScript)
 
         # executor.submit(backgroundAnalyse,nId)
-        backgroundAnalyse(nPluginScript, nClientScript, nId, nArgs)
+        # backgroundAnalyse(nPluginScript, nClientScript, nId, nArgs)
+        executor.submit(backgroundAnalyse, nId, nPluginScript, nClientScript, nPath, nArgs)
+        # backgroundAnalyse(nId, nPluginScript, nClientScript, nPath, nArgs)
 
-        result={'id':nId,'status':'1', 'action': 'symbolic'}
+        result={'id':nId,'status':'ok', 'action': 'symbolic'}
         return jsonify(result)
     except KeyError:
         result={'id':nId,'status':'KeyError'}
@@ -225,5 +496,45 @@ def symbolic():
         return jsonify(result)
 
 
+
+
+
+def load_dict(filename=store_file):
+    global targetDict
+    f = open(filename,'rb')
+    tmp=pickle.load(f)
+    if type(tmp).__name__=='dict':
+        targetDict=tmp
+    f.close()
+
+@atexit.register
+def store_dict(filename=store_file):
+    global targetDict
+    f = open(filename,'wb')
+    pickle.dump(targetDict,f)
+    f.close()
+
+def net():
+    '''
+    获取网卡信息
+    '''
+    netcard_info = None
+    info = psutil.net_if_addrs()
+    for k,v in info.items():
+        for item in v:
+            print(item)
+            if item[0] == 2 and not item[1]=='127.0.0.1' and item[1].find('10.38.')!=-1:
+                netcard_info=(k,item[1])
+    print(netcard_info)
+    return netcard_info
+
+def init_setting():
+    global WORK_URL
+    load_dict(store_file)
+    logging.basicConfig(level=logging.DEBUG)
+    work_ip=net()[1]
+    WORK_URL="http://%s:%d/"%(work_ip,26000)
+
 if __name__ == '__main__':
-     app.run(host='0.0.0.0',port='8000',debug=True)
+    init_setting()
+    app.run(host='0.0.0.0',port='1080',debug=True)
--- nsisAnalyse.py
+++ nsisAnalyse.py
@@ -12,26 +12,26 @@ import json
 
 from multiprocessing import Pool
 
-import pdb
 
 RESULT_FILE = '/tmp/result.txt'
 
 LOGGER = None
 
-
-# Specify the path of triton 
-PRE_COMMAND = '/home/test/pin-2.14-71313-gcc.4.4.7-linux/source/tools/Triton/build/triton ' +  ' '
+# Specify the path of triton
+PRE_COMMAND = '/home/work/pin-2.14-71313-gcc.4.4.7-linux/source/tools/Triton/build/triton ' +  ' '
 
 # Specify the path of triton plugin
-LOCAL_PLUGIN_PATH = './analysePlugin.py'
-LOCAL_CLIENT_PATH = './client.py'
+# LOCAL_PLUGIN_PATH = './temp_analyse_script.py'
+
+# LOCAL_CLIENT_PATH = './temp_client_script.py'
 
 LOG_FILE = 'nsisAnalyse.log'
+
 
 def setLogger():
 
     global LOGGER
-    
+
     baseName = os.path.splitext(os.path.basename(__file__))[0]
 
     LOGGER = logging.getLogger(baseName)
@@ -56,11 +56,12 @@ def setLogger():
 def py2Download(url, filePath):
     import urllib2
     f = urllib2.urlopen(url)
- 
+
     # Open our local file for writing
     with open(filePath, "wb") as local_file:
         local_file.write(f.read())
- 
+
+
 
 def py3Download(url, filePath):
     import urllib.request
@@ -68,23 +69,19 @@ def py3Download(url, filePath):
 
 
 def downloader(url, filePath):
-    try:
-        if sys.version_info.major == 2:
-            py2Download(url, filePath)
-        else:
-            py3Download(url, filePath)
+    if sys.version_info.major == 2:
+        py2Download(url, filePath)
+    else:
+        py3Download(url, filePath)
 
-    except Exception as e:
-        LOGGER.error('Exception: %s', str(e))
-        LOGGER.error ("%s download failed!" , url)
-        return None
-    return filePath
 
 def readFile(filePath):
     with open(filePath, 'r') as f:
         return f.read()
 
-def formatList(listData):
+
+def formatBranch(branch):
+
     flippingStyle = {
         "normal": {
         "color": "red"
@@ -93,69 +90,97 @@ def formatList(listData):
 
     passedStyle = {
         "normal": {
-        "color": "red"
+        "color": "green"
         }
     }
 
 
+    # Parent node
+    node1 = {}
+    node1['name'] = hex(branch['srcAddr'])
+    node1['itemStyle'] = passedStyle
+    link = {}
+
+    # Child nodes that have passed
+    node2 = {}
+    node2['name'] = hex(branch['passedAddr'])
+    node2['itemStyle'] = passedStyle
+
+    # Child nodes to flip
+    node3 = {}
+    node3['name'] = hex(branch['flippingAddr'])
+    node3['itemStyle'] = flippingStyle
+
+    # The path that has been passed
+    link1 = {}
+    link1['source'] = hex(branch['srcAddr'])
+    link1['target'] = hex(branch['passedAddr'])
+    link1['name'] = ''
+
+    # The path that need to be flipped over
+    link2 = {}
+    link2['source'] = hex(branch['srcAddr'])
+    link2['target'] = hex(branch['flippingAddr'])
+    link2['name'] = str(branch['flippingCondition'])
+
+    # Nodes
+    branchNodes=[]
+    nodes = [node1, node2, node3]
+    branchNodes.extend(nodes)
+
+    # Links
+    branchLinks=[]
+    links = [link1, link2]
+    branchLinks.extend(links)
+
+    branch = {}
+    branch['data'] = branchNodes
+    branch['links'] = branchLinks
+
+    return branch
+
+
+
+# Format the result of symbolic execution
+def formatResult(listData):
+
     branchs = []
     for branch in listData:
         if isinstance(branch, dict):
-            node1 = {} 
-            node1['name'] = hex(branch['srcAddr'])
-            node1['itemStyle'] = passedStyle
-            link = {}
-
-            node2 = {} 
-            node2['name'] = hex(branch['passedAddr'])
-            node2['itemStyle'] = passedStyle
-
-            node3 = {} 
-            node3['name'] = hex(branch['flippingAddr'])
-            node3['itemStyle'] = flippingStyle
-
-            link1 = {}
-            link2 = {}
-            link1['source'] = hex(branch['srcAddr'])
-            link1['target'] = hex(branch['passedAddr'])
-            link1['name'] = str(branch['flippingCondition'])
-
-            link2['source'] = hex(branch['srcAddr'])
-            link2['target'] = hex(branch['flippingAddr'])
-            link2['name'] = str(branch['flippingCondition'])
-
-
-            branchNodes=[]
-            nodes = [node1, node2, node3]
-            branchNodes.extend(nodes)
-
-            branchLinks=[]
-            links = [link1, link2]
-            branchLinks.extend(links)
-
-            branch = {}
-            branch['data'] = branchNodes
-            branch['links'] = branchLinks
+            try:
+                branch = formatBranch(branch)
+            except Exception as e:
+                LOGGER.error(str(e))
+                raise RuntimeError('The result is malformed !')
 
             branchs.append(branch)
 
     return branchs
 
-def formatResult(filePath):
-    branchs = list()
-    data = readFile(filePath)
-    try:
-        branchList = ast.literal_eval(data)
-        if isinstance(branchList, list):
-            branchs = formatList(branchList)
-        
-        else:
-            return None
-    except Exception as e:
-        print(e)
 
-    return branchs
- 
+def getResult(filePath):
+
+
+    data = readFile(filePath)
+    result = data
+    try:
+        # List type string to list object
+        branchList = ast.literal_eval(data)
+    except Exception as e:
+        LOGGER.info(str(e))
+
+    else:
+        if isinstance(branchList, list):
+            try:
+                branchs = formatResult(branchList)
+            except Exception as e:
+                LOGGER.info(str(e))
+
+            result = branchs
+
+    finally:
+        return result
+
 
 
 # Execute external command
@@ -176,12 +201,6 @@ def execCmdReout(command, keepRunning = True, output = True,  returnCode = False
 
 
 
-def  writeFile(data, filePath):
-    with open(filePath, 'w+') as the_file:
-        the_file.write(data)
-
-
-
 # Instrumentation and analyze the program
 def analyse(pluginPath, programPath, args=''):
 
@@ -190,19 +209,20 @@ def analyse(pluginPath, programPath, args=''):
 
     try:
 
-        returncode,output = execCmdReout(command, output=False, returnCode=True)
-        if returncode != 0:
-            LOGGER.error(output)
-            LOGGER.error('[ %s ] exec failed!', command)
-            return None
-	                
+        returncode, output = execCmdReout(command, output=False, returnCode=True)
 
     except Exception as e:
-        LOGGER.error('Exception: %s', str(e))
+        LOGGER.error(str(e))
         LOGGER.error('[ %s ] exec failed!', command)
-        return None
+        raise RuntimeError('Trion instrumentation failed !')
+
+    if returncode != 0:
+        LOGGER.error(output)
+        LOGGER.error('[ %s ] exec failed!', command)
+        raise RuntimeError('Trion instrumentation failed !')
 
     return output
+
 
 
 def clientInput(clientScript):
@@ -211,76 +231,78 @@ def clientInput(clientScript):
 
     try:
 
-        returncode,output = execCmdReout(command, output=False, returnCode=True)
-        if returncode != 0:
-            LOGGER.error(output)
-            LOGGER.error('[ %s ] exec failed!', command)
-            return None
-	                
+        returncode, output = execCmdReout(command, output=False, returnCode=True)
+
 
     except Exception as e:
-        LOGGER.error('Exception: %s', str(e))
+        LOGGER.error(str(e))
         LOGGER.error('[ %s ] exec failed!', command)
-        return None
+        raise RuntimeError('Client failed to send data !')
+
+    if returncode != 0:
+        LOGGER.error(output)
+        LOGGER.error('[ %s ] exec failed!', command)
+        raise RuntimeError('Client failed to send data !')
 
     return output
 
- 
 
 
-def autoAnalyse(analyseScriptPath, clientScriptPath, programPath, args=None):
+def autoAnalyse(analyseScriptPath, clientScriptPath, programPath, args=''):
 
-    pool = Pool(processes=2)
-    result_server = []
-    result_client = []
 
     plugin_url = analyseScriptPath
     client_url = clientScriptPath
 
-    pluginPath = LOCAL_PLUGIN_PATH
-    clientPath = LOCAL_CLIENT_PATH
-    if None == downloader(plugin_url, pluginPath):
-        LOGGER.error ("downloader failed!")
+
+    pluginPath = './temp_analyse_script.py'
+    clientPath = './temp_client_script.py'
+
+    # Download Triton plugin
+    try:
+        downloader(plugin_url, pluginPath)
+    except Exception as e:
+        LOGGER.error (str(e))
+        LOGGER.error ("plugin_url <%s>  downloader failed!" % (plugin_url))
         return None
-    if None == downloader(client_url, clientPath):
-        LOGGER.error ("downloader failed!")
+
+    try:
+        downloader(client_url, clientPath)
+    except Exception as e:
+        LOGGER.error (str(e))
+        LOGGER.error ("client_url <%s>  downloader failed!" % (plugin_url))
         return None
 
-    if args:
-        result_server.append(pool.apply_async(analyse, args=(pluginPath,programPath, args,)))
-    else:
-        result_server.append(pool.apply_async(analyse, args=(pluginPath,programPath,)))
+    pool = Pool(processes=2)
+    serverResult = []
+    clientResult = []
 
-    time.sleep(20)
-    result_client.append(pool.apply_async(clientInput, args=(clientPath,)))
+    try:
+        serverResult.append(pool.apply_async(analyse, args=(pluginPath,programPath, args,)))
 
-    pool.close()
-    pool.join()
+        # Wait for the program under test to start
+        time.sleep(20)
 
-    if os.path.isfile(RESULT_FILE):
-        result = formatResult(RESULT_FILE)
-    else:
-        LOGGER.error('autoAnalyse failed!')
-        
+        # Execute script and send data
+        clientResult.append(pool.apply_async(clientInput, args=(clientPath,)))
+
+        pool.close()
+        pool.join()
+    except Exception as e:
+        LOGGER.error(e)
+        LOGGER.error("Symbolic failed!")
+
+    time.sleep(5)
+
+    if not os.path.isfile(RESULT_FILE):
+        return None
+
+    result = getResult(RESULT_FILE)
+    print(result)
 
     return result
-    print(result_server[0], result_client[0])
 
 
-        
+
 setLogger()
 
-
-# data = formatResult(RESULT_FILE)
-# print(data)
-
-# if __name__ == "__main__":
-# 
-#     setLogger()
-#     # pdb.set_trace()
-#     result = autoAnalyse('http://127.0.0.1:8000/template1.py', '/home/test/soft/self_test/server')
-#     if None == result:
-#         LOGGER.debug('failed!')
-#         time.sleep(3)
-#     print(result)
-#     time.sleep(3)
